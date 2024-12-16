877093ae1edb68a039ce799800e6a0168f1095bf
ffXder <fjfajardo0624@gmail.com>
2024-12-16 18:14:16 +0800

    Revert "first commit"
    This reverts commit 65f77fb4a4081a9ecd9a931d06247c4f129fa59b.

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const buttonPopUp = document.querySelector('.btnLogin-popup');
+const btnPopup = document.querySelector('.btnLogin-popup'); 
 const closeIcon = document.querySelector('.close-icon');
 
-registerLink.addEventListener('click', ()=> {
+loginLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
-
-buttonPopUp.addEventListener('click', ()=> {
+btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
 });
 
